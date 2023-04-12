38aa4cfc5b1ee499010d3cda35717f6fd0266b4b
nam3762 <nam37623762@gmail.com>
2023-04-12 15:50:07 +0900

    2023-04-12 15:50

---
+++ Project9_2/app/src/main/java/com/example/project9_2/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.project9_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton ibZoomin, ibZoomout, ibRotate, ibBright, ibDark, ibGray;
+    MyGraphicView graphicView;
+
+    static float scaleX = 1, scaleY = 1;
+    static float angle = 0;
+    static float color = 1;
+    static float satur = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("미니 포토샵");
+
+        LinearLayout pictureLayout = (LinearLayout)findViewById(R.id.pictureLayout);
+        graphicView = (MyGraphicView) new MyGraphicView(this);
+        pictureLayout.addView(graphicView);
+
+        clickIcons();
+    }
+
+    private void clickIcons() {
+        ibZoomin = (ImageButton)findViewById(R.id.ibZoomin);
+        ibZoomin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scaleX += 0.2f;
+                scaleY += 0.2f;
+                graphicView.invalidate();
+            }
+        }); // 확대아이콘 클릭리스너
+
+        ibZoomout = (ImageButton)findViewById(R.id.ibZoomout);
+        ibZoomout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scaleX -= 0.2f;
+                scaleY -= 0.2f;
+                graphicView.invalidate();
+            }
+        }); // 축소아이콘 클릭리스너
+
+        ibRotate = (ImageButton)findViewById(R.id.ibRotate);
+        ibRotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                angle += 20;
+                graphicView.invalidate();
+            }
+        }); // 회전아이콘 클릭리스너
+
+        ibBright = (ImageButton)findViewById(R.id.ibBright);
+        ibBright.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                color += 0.2f;
+                graphicView.invalidate();
+            }
+        }); // 밝기아이콘 클릭리스너
+
+        ibDark = (ImageButton)findViewById(R.id.ibDark);
+        ibDark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                color -= 0.2f;
+                graphicView.invalidate();
+            }
+        }); // 어둡게아이콘 클릭리스너
+
+        ibGray = (ImageButton)findViewById(R.id.ibGray);
+        ibGray.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (satur == 0) {
+                    satur = 1;
+                }
+                else satur = 0;
+                graphicView.invalidate();
+            }
+        }); // 흑백아이콘 클릭리스너
+    }
+
+    private static class MyGraphicView extends View {
+        public MyGraphicView(Context context) {
+            super(context);
+        }
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            int cenX = this.getWidth() / 2;
+            int cenY = this.getHeight() / 2;
+            canvas.scale(scaleX, scaleY, cenX, cenY);
+            canvas.rotate(angle, cenX, cenY);
+
+            Paint paint = new Paint();
+            float[] array = {
+                    color, 0, 0, 0, 0,
+                    0, color, 0, 0, 0,
+                    0, 0, color, 0, 0,
+                    0, 0, 0, 1, 0
+            };
+            ColorMatrix cm = new ColorMatrix(array);
+
+            if(satur == 0)
+                cm.setSaturation(satur);
+
+            paint.setColorFilter(new ColorMatrixColorFilter(cm)); // paint 이게 satur 밑에 있어야 작동함!
+
+            Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.pastel);
+            int picX = (this.getWidth() - picture.getWidth()) / 2;
+            int picY = (this.getHeight() - picture.getHeight()) / 2;
+            canvas.drawBitmap(picture, picX, picY, paint);
+            picture.recycle();
+        }
+    }
+}
